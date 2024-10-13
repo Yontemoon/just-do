8a871b6d64e2ada44376335163849b02c23530f9
YonteMoon <monteyoon1998@gmail.com>
2024-10-13 15:23:37 -0700

    Change file structure for signup page

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -12,8 +12,8 @@
 
 import { Route as rootRoute } from './routes/__root'
 import { Route as SignupImport } from './routes/signup'
+import { Route as SigninImport } from './routes/signin'
 import { Route as IndexImport } from './routes/index'
-import { Route as SigninIndexImport } from './routes/signin/index'
 
 // Create/Update Routes
 
@@ -22,13 +22,13 @@ const SignupRoute = SignupImport.update({
   getParentRoute: () => rootRoute,
 } as any)
 
-const IndexRoute = IndexImport.update({
-  path: '/',
+const SigninRoute = SigninImport.update({
+  path: '/signin',
   getParentRoute: () => rootRoute,
 } as any)
 
-const SigninIndexRoute = SigninIndexImport.update({
-  path: '/signin/',
+const IndexRoute = IndexImport.update({
+  path: '/',
   getParentRoute: () => rootRoute,
 } as any)
 
@@ -43,18 +43,18 @@ declare module '@tanstack/react-router' {
       preLoaderRoute: typeof IndexImport
       parentRoute: typeof rootRoute
     }
+    '/signin': {
+      id: '/signin'
+      path: '/signin'
+      fullPath: '/signin'
+      preLoaderRoute: typeof SigninImport
+      parentRoute: typeof rootRoute
+    }
     '/signup': {
       id: '/signup'
       path: '/signup'
       fullPath: '/signup'
       preLoaderRoute: typeof SignupImport
-      parentRoute: typeof rootRoute
-    }
-    '/signin/': {
-      id: '/signin/'
-      path: '/signin'
-      fullPath: '/signin'
-      preLoaderRoute: typeof SigninIndexImport
       parentRoute: typeof rootRoute
     }
   }
@@ -64,42 +64,42 @@ declare module '@tanstack/react-router' {
 
 export interface FileRoutesByFullPath {
   '/': typeof IndexRoute
+  '/signin': typeof SigninRoute
   '/signup': typeof SignupRoute
-  '/signin': typeof SigninIndexRoute
 }
 
 export interface FileRoutesByTo {
   '/': typeof IndexRoute
+  '/signin': typeof SigninRoute
   '/signup': typeof SignupRoute
-  '/signin': typeof SigninIndexRoute
 }
 
 export interface FileRoutesById {
   __root__: typeof rootRoute
   '/': typeof IndexRoute
+  '/signin': typeof SigninRoute
   '/signup': typeof SignupRoute
-  '/signin/': typeof SigninIndexRoute
 }
 
 export interface FileRouteTypes {
   fileRoutesByFullPath: FileRoutesByFullPath
-  fullPaths: '/' | '/signup' | '/signin'
+  fullPaths: '/' | '/signin' | '/signup'
   fileRoutesByTo: FileRoutesByTo
-  to: '/' | '/signup' | '/signin'
-  id: '__root__' | '/' | '/signup' | '/signin/'
+  to: '/' | '/signin' | '/signup'
+  id: '__root__' | '/' | '/signin' | '/signup'
   fileRoutesById: FileRoutesById
 }
 
 export interface RootRouteChildren {
   IndexRoute: typeof IndexRoute
+  SigninRoute: typeof SigninRoute
   SignupRoute: typeof SignupRoute
-  SigninIndexRoute: typeof SigninIndexRoute
 }
 
 const rootRouteChildren: RootRouteChildren = {
   IndexRoute: IndexRoute,
+  SigninRoute: SigninRoute,
   SignupRoute: SignupRoute,
-  SigninIndexRoute: SigninIndexRoute,
 }
 
 export const routeTree = rootRoute
@@ -115,18 +115,18 @@ export const routeTree = rootRoute
       "filePath": "__root.tsx",
       "children": [
         "/",
-        "/signup",
-        "/signin/"
+        "/signin",
+        "/signup"
       ]
     },
     "/": {
       "filePath": "index.tsx"
     },
+    "/signin": {
+      "filePath": "signin.tsx"
+    },
     "/signup": {
       "filePath": "signup.tsx"
-    },
-    "/signin/": {
-      "filePath": "signin/index.tsx"
     }
   }
 }
--- src/routes/signin.tsx
+++ src/routes/signin.tsx
@@ -3,56 +3,56 @@ import {
   Link,
   redirect,
   useNavigate,
-} from "@tanstack/react-router";
-import { useForm } from "@tanstack/react-form";
-import Input from "@/components/Input";
-import FormField from "@/components/FormField";
-import Label from "@/components/Label";
-import Button from "@/components/Button";
-import { authAction, auth } from "@/helper/auth";
-import { useState } from "react";
-import { SignInSchema } from "@/types/z.types";
-import { zodValidator } from "@tanstack/zod-form-adapter";
-import FieldInfo from "@/components/FieldInfo";
-import { dateUtils } from "@/helper/utils";
+} from '@tanstack/react-router'
+import { useForm } from '@tanstack/react-form'
+import Input from '@/components/Input'
+import FormField from '@/components/FormField'
+import Label from '@/components/Label'
+import Button from '@/components/Button'
+import { authAction, auth } from '@/helper/auth'
+import { useState } from 'react'
+import { SignInSchema } from '@/types/z.types'
+import { zodValidator } from '@tanstack/zod-form-adapter'
+import FieldInfo from '@/components/FieldInfo'
+import { dateUtils } from '@/helper/utils'
 
-export const Route = createFileRoute("/signin/")({
+export const Route = createFileRoute('/signin')({
   beforeLoad: () => {
     if (auth.getUserId()) {
       throw redirect({
-        to: "/",
-        search: { display: "all", date: dateUtils.getToday(), date_all: false },
-      });
+        to: '/',
+        search: { display: 'all', date: dateUtils.getToday(), date_all: false },
+      })
     }
   },
 
   component: SignIn,
-});
+})
 
 function SignIn() {
-  const [serverValidation, setServerValidation] = useState("");
-  const navigate = useNavigate();
+  const [serverValidation, setServerValidation] = useState('')
+  const navigate = useNavigate()
 
   const form = useForm({
     defaultValues: {
-      email: "",
-      password: "",
+      email: '',
+      password: '',
     },
     onSubmit: async (values) => {
       try {
-        await authAction.login(values.value.email, values.value.password);
+        await authAction.login(values.value.email, values.value.password)
 
         navigate({
-          to: "/",
+          to: '/',
           search: {
-            display: "all",
+            display: 'all',
             date: dateUtils.getToday(),
             date_all: false,
           },
-        });
+        })
       } catch (error) {
-        setServerValidation("Email or password is invalid.");
-        console.error(error);
+        setServerValidation('Email or password is invalid.')
+        console.error(error)
       }
     },
     validatorAdapter: zodValidator(),
@@ -60,7 +60,7 @@ function SignIn() {
       onChangeAsyncDebounceMs: 500,
       onChangeAsync: SignInSchema,
     },
-  });
+  })
 
   return (
     <div className="w-96 m-0 mx-auto max-w-full">
@@ -68,9 +68,9 @@ function SignIn() {
 
       <form
         onSubmit={(e) => {
-          e.preventDefault();
-          e.stopPropagation();
-          form.handleSubmit();
+          e.preventDefault()
+          e.stopPropagation()
+          form.handleSubmit()
         }}
         className="grid gap-3"
       >
@@ -112,7 +112,7 @@ function SignIn() {
           selector={(state) => [state.canSubmit, state.isSubmitting]}
           children={([canSubmit, isSubmitting]) => (
             <Button type="submit" className="w-full" disabled={!canSubmit}>
-              {isSubmitting ? "Loading..." : "Login"}
+              {isSubmitting ? 'Loading...' : 'Login'}
             </Button>
           )}
         />
@@ -122,5 +122,5 @@ function SignIn() {
         </div>
       </form>
     </div>
-  );
+  )
 }
